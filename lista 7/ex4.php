306e2d7b2b07cb0260e23a1465286d8d672e05fe
joaoSouzaafk <joao.paladino@edu.senai.br>
2026-09-02 11:21:41 -0300

    Add files via upload

---
+++ lista 7/ex4.php
@@ -0,0 +1,15 @@
+<?php
+
+function verificarIdade($idade){
+    if($idade <16){
+        return "Não pode votar";
+    }elseif($idade <18){
+        return "Voto Opcional";
+    }else{
+        return "Voto Obrigatorio";
+    }
+}
+
+echo verificarIdade(17);
+
+?>
